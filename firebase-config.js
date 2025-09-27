451d03c222748a5ac79a7d295faf13b5da12d51d
mac hax <almalikakbaradly@gmail.com>
2025-09-27 20:49:54 +0700

    menambahkan lampiran pada absensi

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-storage.js";
 
 // Config dari Firebase Console
 const firebaseConfig = {
@@ -16,3 +17,4 @@ const firebaseConfig = {
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
